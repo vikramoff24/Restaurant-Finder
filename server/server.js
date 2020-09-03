c0a8ff985b25264fee431314c4f262892e3c1c51
vikramoff24 <vikram.senthil@outlook.com>
2020-09-03 20:50:20 +0530

    Setting Routes and Middlewares

--- server/server.js
+++ server/server.js
@@ -2,9 +2,17 @@ require("dotenv").config()
 const express=require("express")
 const app=express()
 
+//Middleware-should be placed top
+app.use((req,res,next)=>
+{
+    console.log("Yeah its middleware");
+    next();
+});
+
 // Get all Restaurants
 app.get("/api/v1/restaurants",(req,res)=>
 {
+    console.log("route handler");
     //changning the status code
     res.status(200).json({
         status:"success",
@@ -12,8 +20,21 @@ app.get("/api/v1/restaurants",(req,res)=>
         restaurant:["mcdonals","kfc"]
         }
     });
-})
+});
 
+//Get a Restaurant
+
+app.get("/api/v1/restaurants/:id",(req,res)=>
+{
+console.log(req.params);
+});
+
+//Create a Restaurant
+
+app.post("/api/v1/restaurants",(req,res)=>
+{
+console.log(req);
+});
 
 const port=process.env.PORT||3001;
 app.listen(port,()=>{
