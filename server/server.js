abf709871c02c57ed515d26f4fa2038482ce10ab
Vikram S <vikram.senthil@outlook.com>
2020-09-22 18:54:14 +0530

    Added Response for the requests and Added Delete Request

--- server/server.js
+++ server/server.js
@@ -37,20 +37,52 @@ app.get("/api/v1/restaurants",(req,res)=>
 app.get("/api/v1/restaurants/:id",(req,res)=>
 {
 console.log(req.params.id);
+res.status(200).json({
+    status:"success",
+    data:{
+    restaurant:["kfc"]
+    }
+})
 });
 
 //Create a Restaurant
 app.post("/api/v1/restaurants",(req,res)=>
 {
 console.log(req.body);
+res.status(201).json(
+    {
+        status:"success",
+        data:{
+        restaurant:["kfc"]
+    }
+
 });
+});
+
 
 //Udate Restaurants
 app.put("/api/v1/restaurants/:id",(req,res)=>{
 console.log(req.params.id)
-console.log(req.body);
+console.log(req.body)
+res.status(200).json({
+        status:"success",
+        data:{
+        restaurant:["kfc"]
+        }
+    }
+    )
 }
 )
+
+//Delete Restaurant
+app.delete("/api/v1/restaurants/:id",(req,res)=>{
+
+res.status(204).json({
+            status:"success"
+            }
+        )
+    });
+    
 
 const port=process.env.PORT||3001;
 app.listen(port,()=>{
