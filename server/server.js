395581c09de365bb660c9a5b99190d446bf7a46e
Vikram S <vikram.senthil@outlook.com>
2020-09-22 18:37:28 +0530

    Updating Restaurants using Put Request

--- server/server.js
+++ server/server.js
@@ -4,18 +4,21 @@ const morgan=require("morgan");
 const app=express();
 
 //Middleware-should be placed top //we can have any number of middleware
-app.use(morgan("tiny")); //third party middle ware. //next function is already passed
+// app.use(morgan("tiny")); //third party middle ware. //next function is already passed
 
 // app.use((req,res,next)=>
 // {
-// console.log(1st middleware);
-// next();//tell to send the flow to next middleware or final route handler 
+// console.log("1st middleware");
+// // next();//tell to send the flow to next middleware or final route handler 
 // });
 //app.use((req,res,next)=>
 // {
 // console.log(2nd middleware);
 // next();//tell to send the flow to next middleware or final route handler 
 // })
+
+app.use(express.json());
+
 
 // Get all Restaurants
 app.get("/api/v1/restaurants",(req,res)=>
@@ -33,14 +36,21 @@ app.get("/api/v1/restaurants",(req,res)=>
 //Get a Restaurant
 app.get("/api/v1/restaurants/:id",(req,res)=>
 {
-console.log(req.params);
+console.log(req.params.id);
 });
 
 //Create a Restaurant
 app.post("/api/v1/restaurants",(req,res)=>
 {
-console.log(req);
+console.log(req.body);
 });
+
+//Udate Restaurants
+app.put("/api/v1/restaurants/:id",(req,res)=>{
+console.log(req.params.id)
+console.log(req.body);
+}
+)
 
 const port=process.env.PORT||3001;
 app.listen(port,()=>{
