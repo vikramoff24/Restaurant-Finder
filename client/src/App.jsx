b64bf276d55bb2d34bc656b2cb97f1b93a3995a7
Vikram S <vikram.senthil@outlook.com>
2020-09-28 10:24:09 +0530

    Setted up React Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import UpdatePage from './routes/UpdatePage';
+import Home from './routes/Home';
+import RestaurantdetailPage from './routes/RestaurantdetailPage';
 const App =() =>
 {
-    return <div>App</div>
-}
+    return <div>
+    <Router>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/restaurants/:id/update" component={UpdatePage}/>
+        <Route exact path="/restaurants/:id" component={RestaurantdetailPage}/>
+    </Router>
+    </div>
+};
 export default App;
---
+++ client/src/routes/RestaurantdetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RestaurantdetailPage = () => {
+    return (
+        <div>
+            detail page
+        </div>
+    )
+}
+
+export default RestaurantdetailPage
