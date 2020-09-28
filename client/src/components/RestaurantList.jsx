9c341c4d9c9358156ea8f38d864951e96317a2ae
Vikram S <vikram.senthil@outlook.com>
2020-09-28 20:11:05 +0530

    Setting Up state of Restaurant

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -1,38 +1,41 @@
-import React ,{useEffect} from 'react'
+import React, { useEffect, useContext } from 'react'
 import RestaurantFinder from "../apis/RestaurantFinder"
+import { RestaurantsContext } from '../context/RestaurantsContext'
 //useEffect is a Hook which retrives the data from backend as soon as the component pops up;
-const RestaurantList = () => {
+const RestaurantList = (props) => {
+    const { restaurants, setRestaurants } = useContext(RestaurantsContext)
+    useEffect(() =>//it should not not return anything.
+    {
+        const fetchData = async () =>//it is used to avoid returning of promises from  use effect as it doesn't like. Promises are returnef by this function
+        {
+            try {
+                const response = await RestaurantFinder.get("/");
 
-useEffect(()=>
-    {
-        const fetchData=async()=>//it is used to avoid returning of promises from  use effect as it doesn't like. Promises are returnef by this function
-    {
-try{
-const response= await RestaurantFinder.get("/");
-console.log(response);
-}catch(err)
-{
-console.log(err);
-}};
-fetchData();
-    },[])
+                setRestaurants(response.data.data.restaurant)
+
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        fetchData();
+    }, [])
     //empty dependency array where it makes to run only one time when it get pops up, does not run  every time it renders)
-return (
+    return (
         <div className="list-group">
-          <table className="table table-hover table-dark">
-              <thead>
-                  <tr className="bg-primary">
-                     
-                      <th scope="col">Restaurant</th>
-                      <th scope="col">Location</th>
-                      <th scope="col">Price Range</th>
-                      <th scope="col">Rating</th>
-                      <th scope="col">Edit</th>
-                      <th scope="col">Delete</th>
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
+            <table className="table table-hover table-dark">
+                <thead>
+                    <tr className="bg-primary">
+
+                        <th scope="col">Restaurant</th>
+                        <th scope="col">Location</th>
+                        <th scope="col">Price Range</th>
+                        <th scope="col">Rating</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <tr>
                       <td>mcdonalds</td>
                       <td>chennai</td>
                       <td>$$</td>
@@ -55,9 +58,9 @@ return (
                       <td>Rating</td>
                       <td><button className="btn btn-warning">Update</button></td>
                       <td><button className="btn btn-danger">Delete</button></td>
-                  </tr>
-              </tbody>
-          </table>
+                  </tr> */}
+                </tbody>
+            </table>
         </div>
     )
 }
